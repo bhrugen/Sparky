2012a4feba2ad3079d7985bdc6409e1dc72f1fa3
Bhrugen Patel <bhrugen1990@gmail.com>
2021-06-18 16:58:41 -0500

    Section 2 - Failed Test Example

---
+++ SparkyMSTest/CalculatorMSTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyMSTest
+{
+    [TestClass]
+    public class CalculatorMSTests
+    {
+        [TestMethod]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+            Calculator calc = new();
+
+            //Act
+            int result = calc.AddNumbers(10, 20);
+
+
+            //Assert
+            Assert.AreEqual(30,result);
+        }
+    }
+}
