d0c91f9124d4483f3495b83dc67699d8b34eb9db
Bhrugen Patel <bhrugen1990@gmail.com>
2021-06-18 17:10:13 -0500

    Section 3 - First NUnit Test

---
+++ SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    [TestFixture]
+    public class CalculatorNUnitTests
+    {
+        [Test]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+            Calculator calc = new();
+
+            //Act
+            int result = calc.AddNumbers(10, 20);
+
+
+            //Assert
+            Assert.AreEqual(30, result);
+        }
+    }
+}
