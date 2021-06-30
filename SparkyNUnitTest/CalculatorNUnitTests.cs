c50af91788df23a5e09c0c0459154cc0a4ec8e6e
Bhrugen Patel <bhrugen1990@gmail.com>
2021-06-30 13:06:32 -0500

    Section 4 - Collections Helper Method

--- SparkyNUnitTest/CalculatorNUnitTests.cs
+++ SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -85,6 +85,14 @@ namespace Sparky
 
             //Assert
             Assert.That(result, Is.EquivalentTo(expectedOddRange));
+            //Assert.AreEqual( expectedOddRange, result);
+            //Assert.Contains(7, result);
+            Assert.That(result, Does.Contain(7));
+            Assert.That(result, Is.Not.Empty);
+            Assert.That(result.Count, Is.EqualTo(3));
+            Assert.That(result, Has.No.Member(6));
+            Assert.That(result, Is.Ordered);
+            Assert.That(result, Is.Unique);
         }
     }
 }
