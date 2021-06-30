1fabd88f2a68e7f195fa2390ffe8fd73295b1da1
Bhrugen Patel <bhrugen1990@gmail.com>
2021-06-30 12:58:56 -0500

    Section 4 - Asserting Collections

--- Sparky/Calculator.cs
+++ Sparky/Calculator.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Sparky
 {
     public class Calculator
     {
+        public List<int> NumberRange = new();
+
         public int AddNumbers(int a,int b)
         {
             return a + b;
@@ -17,6 +20,19 @@ namespace Sparky
         public bool IsOddNumber(int a)
         {
             return a % 2 != 0;
+        }
+
+        public List<int> GetOddRange(int min, int max)
+        {
+            NumberRange.Clear();
+            for(int i = min; i <= max; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    NumberRange.Add(i);
+                }
+            }
+            return NumberRange;
         }
     }
 }
--- SparkyNUnitTest/CalculatorNUnitTests.cs
+++ SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -72,5 +72,19 @@ namespace Sparky
             Assert.AreEqual(15.9, result,.2);
             //15.7-16.1
         }
+
+
+        [Test]
+        public void OddRanger_InputMinAndMaxRange_ReturnsValidOddNumberRange()
+        {
+            Calculator calc = new();
+            List<int> expectedOddRange = new() { 5, 7, 9 }; //5-10
+
+            //act
+            List<int> result = calc.GetOddRange(5, 10);
+
+            //Assert
+            Assert.That(result, Is.EquivalentTo(expectedOddRange));
+        }
     }
 }
