6b9d35e988440c25b8aa3b031753c3e6c31e2f5c
Bhrugen Patel <bhrugen1990@gmail.com>
2021-07-01 17:31:01 -0500

    Section 4 -  Assignment 2 - Grading Calculator Part 1

---
+++ Sparky/GradingCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    public class GradingCalculator
+    {
+        public int Score { get; set; }
+        public int AttendancePercentage { get; set; }
+        public string GetGrade()
+        {
+            if (Score > 90 && AttendancePercentage > 70)
+                return "A";
+            else
+                if (Score > 80 && AttendancePercentage > 60)
+                return "B";
+            else
+                    if (Score > 60 & AttendancePercentage > 60)
+                return "C";
+            else
+                return "F";
+        }
+    }
+}
