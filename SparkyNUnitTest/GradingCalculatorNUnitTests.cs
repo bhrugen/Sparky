ee421fe02c9a87dd505cd4e22fdf6f0631caff0e
Bhrugen Patel <bhrugen1990@gmail.com>
2021-07-01 17:38:54 -0500

    Section 4 - Assignment 2 - Grading Calculator Part 1 Solution

---
+++ SparkyNUnitTest/GradingCalculatorNUnitTests.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    [TestFixture]
+    public class GradingCalculatorNUnitTests
+    {
+        private GradingCalculator gradingCalculator;
+        [SetUp]
+        public void Setup()
+        {
+            gradingCalculator = new GradingCalculator();
+        }
+
+        [Test]
+        public void GradeCalc_InputScore95Attendance90_GetAGrade()
+        {
+            gradingCalculator.Score = 95;
+            gradingCalculator.AttendancePercentage = 90;
+            string result = gradingCalculator.GetGrade();
+            Assert.That(result, Is.EqualTo("A"));
+        }
+
+        [Test]
+        public void GradeCalc_InputScore85Attendance90_GetBGrade()
+        {
+            gradingCalculator.Score = 85;
+            gradingCalculator.AttendancePercentage = 90;
+            string result = gradingCalculator.GetGrade();
+            Assert.That(result, Is.EqualTo("B"));
+        }
+
+        [Test]
+        public void GradeCalc_InputScore65Attendance90_GetCGrade()
+        {
+            gradingCalculator.Score = 65;
+            gradingCalculator.AttendancePercentage = 90;
+            string result = gradingCalculator.GetGrade();
+            Assert.That(result, Is.EqualTo("C"));
+        }
+
+        [Test]
+        public void GradeCalc_InputScore95Attendance65_GetBGrade()
+        {
+            gradingCalculator.Score = 95;
+            gradingCalculator.AttendancePercentage = 65;
+            string result = gradingCalculator.GetGrade();
+            Assert.That(result, Is.EqualTo("B"));
+        }
+
+        [Test]
+        [TestCase(95,55)]
+        [TestCase(65, 55)]
+        [TestCase(50, 90)]
+        public void GradeCalc_FailsureScenarios_GetFGrade(int score, int attendance)
+        {
+            gradingCalculator.Score = score;
+            gradingCalculator.AttendancePercentage = attendance;
+            string result = gradingCalculator.GetGrade();
+            Assert.That(result, Is.EqualTo("F"));
+        }
+    }
+}
