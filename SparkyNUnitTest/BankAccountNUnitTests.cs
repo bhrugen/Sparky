a1f6a05584df8aac17efacfe55159be83dde3d0c
Bhrugen Patel <bhrugen1990@gmail.com>
2021-07-02 12:56:23 -0500

    Section 5 - MOQ Setup with Conditional Return

--- SparkyNUnitTest/BankAccountNUnitTests.cs
+++ SparkyNUnitTest/BankAccountNUnitTests.cs
@@ -17,14 +17,14 @@ namespace Sparky
         {
             
         }
-        [Test]  
-        public void BankDepositLogFakker_Add100_ReturnTrue()
-        {
-            BankAccount bankAccount = new(new LogFakker());
-            var result = bankAccount.Deposit(100);
-            Assert.IsTrue(result);
-            Assert.That(bankAccount.GetBalance, Is.EqualTo(100));
-        }
+        //[Test]  
+        //public void BankDepositLogFakker_Add100_ReturnTrue()
+        //{
+        //    BankAccount bankAccount = new(new LogFakker());
+        //    var result = bankAccount.Deposit(100);
+        //    Assert.IsTrue(result);
+        //    Assert.That(bankAccount.GetBalance, Is.EqualTo(100));
+        //}
 
         [Test]
         public void BankDeposit_Add100_ReturnTrue()
@@ -39,7 +39,26 @@ namespace Sparky
             Assert.That(bankAccount.GetBalance, Is.EqualTo(100));
         }
 
+        [Test]
+        [TestCase(200,100)]
+        [TestCase(200, 150)]
+        public void BankWithdraw_Withdraw100With200Balance_ReturnsTrue(int balance, int withdraw)
+        {
+            var logMock = new Mock<ILogBook>();
+            logMock.Setup(u => u.LogToDb(It.IsAny<string>())).Returns(true);
+            logMock.Setup(u => u.LogBalanceAfterWithdrawal(It.Is<int>(x=>x>0))).Returns(true);
 
+            BankAccount bankAccount = new(logMock.Object);
+            bankAccount.Deposit(balance);
+            var result = bankAccount.Withrdraw(withdraw);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void BankWithdraw_Withdraw300With200Balance_ReturnsFalse()
+        {
+
+        }
 
     }
 }
