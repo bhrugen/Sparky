b0d17ec27bf924a314d590dcccc6332f7dcfbdf0
Bhrugen Patel <bhrugen1990@gmail.com>
2021-07-02 09:43:58 -0500

    Section 5 - Integration Test Demo

---
+++ SparkyNUnitTest/BankAccountNUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    [TestFixture]
+    public class BankAccountNUnitTests
+    {
+        private BankAccount bankAccount;
+        [SetUp]
+        public void Steup()
+        {
+            bankAccount = new(new LogBook());
+        }
+        [Test]  
+        public void BankDeposit_Add100_ReturnTrue()
+        {
+            var result = bankAccount.Deposit(100);
+            Assert.IsTrue(result);
+            Assert.That(bankAccount.GetBalance, Is.EqualTo(100));
+        }
+
+    }
+}
