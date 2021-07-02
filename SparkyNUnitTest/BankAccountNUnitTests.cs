4fc315504de3e6941dccd4b966ebe5491140bb0e
Bhrugen Patel <bhrugen1990@gmail.com>
2021-07-02 14:17:27 -0500

    Section 5 - MOQ Callbacks

--- SparkyNUnitTest/BankAccountNUnitTests.cs
+++ SparkyNUnitTest/BankAccountNUnitTests.cs
@@ -120,6 +120,24 @@ namespace Sparky
             logMock.Object.LogSeverity = 100;
             Assert.That(logMock.Object.LogSeverity, Is.EqualTo(100));
             Assert.That(logMock.Object.LogType, Is.EqualTo("warning"));
+
+            //callbacks
+            string logTemp = "Hello, ";
+            logMock.Setup(u => u.LogToDb(It.IsAny<string>()))
+                .Returns(true).Callback((string str) => logTemp += str);
+            logMock.Object.LogToDb("Ben");
+            Assert.That(logTemp, Is.EqualTo("Hello, Ben"));
+
+            //callbacks
+            int counter= 5;
+            logMock.Setup(u => u.LogToDb(It.IsAny<string>()))
+                .Callback(() => counter++)
+                .Returns(true)
+                .Callback(() => counter++);
+            logMock.Object.LogToDb("Ben");
+            logMock.Object.LogToDb("Ben");
+            Assert.That(counter, Is.EqualTo(9));
+
         }
 
     }
