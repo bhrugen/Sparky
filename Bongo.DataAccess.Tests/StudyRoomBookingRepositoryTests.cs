4724819834da52b4e49905febfb32e89498e8712
Bhrugen Patel <bhrugen1990@gmail.com>
2021-07-03 16:15:14 -0500

    Section 7 - In Memory Database and Unit Testing

---
+++ Bongo.DataAccess.Tests/StudyRoomBookingRepositoryTests.cs
@@ -0,0 +1,68 @@
+﻿using Bongo.DataAccess.Repository;
+using Bongo.Models.Model;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bongo.DataAccess
+{
+    [TestFixture]
+    public class StudyRoomBookingRepositoryTests
+    {
+        private StudyRoomBooking studyRoomBooking_One;
+        private StudyRoomBooking studyRoomBooking_Two;
+
+        public StudyRoomBookingRepositoryTests()
+        {
+            studyRoomBooking_One = new StudyRoomBooking()
+            {
+                FirstName = "Ben1",
+                LastName = "Spark1",
+                Date = new DateTime(2023, 1, 1),
+                Email = "ben1@gmail.coom",
+                BookingId = 11,
+                StudyRoomId = 1
+            };
+
+            studyRoomBooking_Two = new StudyRoomBooking
+            {
+                FirstName = "Ben2",
+                LastName = "Spark2",
+                Date = new DateTime(2023, 2, 2),
+                Email = "ben2@gmail.coom",
+                BookingId = 22,
+                StudyRoomId = 2
+            };
+        }
+
+        [Test]
+        public void SaveBooking_Booking_One_CheckTheValuesFromDatabase()
+        {
+            //arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "temp_Bongo").Options;
+
+            //act
+            using (var context = new ApplicationDbContext(options))
+            {
+                var repository = new StudyRoomBookingRepository(context);
+                repository.Book(studyRoomBooking_One);
+            }
+
+            //assert
+            using (var context = new ApplicationDbContext(options))
+            {
+                var bookingFromDb = context.StudyRoomBookings.FirstOrDefault(u=>u.BookingId==11);
+                Assert.AreEqual(studyRoomBooking_One.BookingId, bookingFromDb.BookingId);
+                Assert.AreEqual(studyRoomBooking_One.FirstName, bookingFromDb.FirstName);
+                Assert.AreEqual(studyRoomBooking_One.LastName, bookingFromDb.LastName);
+                Assert.AreEqual(studyRoomBooking_One.Email, bookingFromDb.Email);
+                Assert.AreEqual(studyRoomBooking_One.Date, bookingFromDb.Date);
+            }
+        }
+    }
+}
