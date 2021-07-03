f8bdc1678fd7c3a4f85dc4a7ad2c9937e7d32d6e
Bhrugen Patel <bhrugen1990@gmail.com>
2021-07-03 10:48:44 -0500

    Section 6 - Xunit Calculator Part 2

--- SparkyXUnit/CalculatorXUnitTests.cs
+++ SparkyXUnit/CalculatorXUnitTests.cs
@@ -53,44 +53,42 @@ namespace Sparky
             Assert.Equal(expectedResult, result);
         }
 
-        //[Fact]
-        //[TestCase(5.4, 10.5)] //15.9
-        //[TestCase(5.43, 10.53)] // 15.93
-        //[TestCase(5.49, 10.59)] // 16.08
-        //public void AddNumbersDouble_InputTwoDouble_GetCorrectAddition(double a, double b)
-        //{
-        //    //Arrange
-        //    Calculator calc = new();
+        [Theory]
+        [InlineData(5.4, 10.5)] //15.9
+        //[InlineData(5.43, 10.53)] // 15.96
+        //[InlineData(5.49, 10.59)] // 16.08
+        public void AddNumbersDouble_InputTwoDouble_GetCorrectAddition(double a, double b)
+        {
+            //Arrange
+            Calculator calc = new();
 
-        //    //Act
-        //    double result = calc.AddNumbersDouble(a, b);
-
-
-        //    //Assert
-        //    Assert.AreEqual(15.9, result, .2);
-        //    //15.7-16.1
-        //}
+            //Act
+            double result = calc.AddNumbersDouble(a, b);
 
 
-        //[Fact]
-        //public void OddRanger_InputMinAndMaxRange_ReturnsValidOddNumberRange()
-        //{
-        //    Calculator calc = new();
-        //    List<int> expectedOddRange = new() { 5, 7, 9 }; //5-10
+            //Assert
+            Assert.Equal(15.9, result,1);
+            //15.7-16.1
+        }
 
-        //    //act
-        //    List<int> result = calc.GetOddRange(5, 10);
 
-        //    //Assert
-        //    Assert.That(result, Is.EquivalentTo(expectedOddRange));
-        //    //Assert.AreEqual( expectedOddRange, result);
-        //    //Assert.Contains(7, result);
-        //    Assert.That(result, Does.Contain(7));
-        //    Assert.That(result, Is.Not.Empty);
-        //    Assert.That(result.Count, Is.EqualTo(3));
-        //    Assert.That(result, Has.No.Member(6));
-        //    Assert.That(result, Is.Ordered);
-        //    Assert.That(result, Is.Unique);
-        //}
+        [Fact]
+        public void OddRanger_InputMinAndMaxRange_ReturnsValidOddNumberRange()
+        {
+            Calculator calc = new();
+            List<int> expectedOddRange = new() { 5, 7, 9 }; //5-10
+
+            //act
+            List<int> result = calc.GetOddRange(5, 10);
+
+            //Assert
+            Assert.Equal(expectedOddRange, result);
+            Assert.Contains(7, result);
+            Assert.NotEmpty(result);
+            Assert.Equal(3,result.Count);
+            Assert.DoesNotContain(6,result);
+            Assert.Equal(result.OrderBy(u=>u), result);
+            //Assert.That(result, Is.Unique);
+        }
     }
 }
