9b1fc46626ca1db496afae88b52f0201d8110680
Bhrugen Patel <bhrugen1990@gmail.com>
2021-07-03 11:14:13 -0500

    Section 6 - MOQ and Xunit

--- SparkyXUnit/ProductXUnitTests.cs
+++ SparkyXUnit/ProductXUnitTests.cs
@@ -1,37 +1,36 @@
-﻿//using Moq;
-//using NUnit.Framework;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
-//namespace Sparky
-//{
-//    [TestFixture]
-//    public class ProductXUnitTests
-//    {
-//        [Test]
-//        public void GetProductPrice_PlatinumCustomer_ReturnPriceWith20Discount()
-//        {
-//            Product product = new Product() { Price = 50 };
+namespace Sparky
+{
+    public class ProductXUnitTests
+    {
+        [Fact]
+        public void GetProductPrice_PlatinumCustomer_ReturnPriceWith20Discount()
+        {
+            Product product = new () { Price = 50 };
 
-//            var result = product.GetPrice(new Customer() { IsPlatinum = true });
+            var result = product.GetPrice(new Customer() { IsPlatinum = true });
 
-//            Assert.That(result, Is.EqualTo(40));
-//        }
+            Assert.Equal(40,result);
+        }
 
-//        [Test]
-//        public void GetProductPriceMOQAbuse_PlatinumCustomer_ReturnPriceWith20Discount()
-//        {
-//            var customer = new Mock<ICustomer>();
-//            customer.Setup(u => u.IsPlatinum).Returns(true);
+        [Fact]
+        public void GetProductPriceMOQAbuse_PlatinumCustomer_ReturnPriceWith20Discount()
+        {
+            var customer = new Mock<ICustomer>();
+            customer.Setup(u => u.IsPlatinum).Returns(true);
 
-//            Product product = new Product() { Price = 50 };
+            Product product = new Product() { Price = 50 };
 
-//            var result = product.GetPrice(customer.Object);
+            var result = product.GetPrice(customer.Object);
 
-//            Assert.That(result, Is.EqualTo(40));
-//        }
-//    }
-//}
+            Assert.Equal(40,result);
+        }
+    }
+}
