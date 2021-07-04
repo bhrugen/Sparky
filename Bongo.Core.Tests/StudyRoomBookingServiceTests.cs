4b9e12e9d75c5d77a1624adc2ea316e79c9be105
Bhrugen Patel <bhrugen1990@gmail.com>
2021-07-04 09:07:31 -0500

    Section 7 - Bongo Core Return Code TestCase

--- Bongo.Core.Tests/StudyRoomBookingServiceTests.cs
+++ Bongo.Core.Tests/StudyRoomBookingServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using Bongo.Core.Services;
 using Bongo.DataAccess.Repository.IRepository;
 using Bongo.Models.Model;
+using Bongo.Models.Model.VM;
 using Moq;
 using NUnit.Framework;
 using System;
@@ -86,6 +87,31 @@ namespace Bongo.Core
             Assert.AreEqual(_request.Email, savedStudyRoomBooking.Email);
             Assert.AreEqual(_request.Date, savedStudyRoomBooking.Date);
             Assert.AreEqual(_availableStudyRoom.First().Id, savedStudyRoomBooking.StudyRoomId);
+        }
+
+        [Test]
+        public void StudyRoomBookingResultCheck_InputRequest_ValuesMatchInResult()
+        {
+            StudyRoomBookingResult result = _bookingService.BookStudyRoom(_request);
+
+            Assert.NotNull(result);
+            Assert.AreEqual(_request.FirstName, result.FirstName);
+            Assert.AreEqual(_request.LastName, result.LastName);
+            Assert.AreEqual(_request.Email, result.Email);
+            Assert.AreEqual(_request.Date, result.Date);
+        }
+
+        [TestCase(true, ExpectedResult =StudyRoomBookingCode.Success)]
+        [TestCase(false, ExpectedResult = StudyRoomBookingCode.NoRoomAvailable)]
+        public StudyRoomBookingCode ResultCodeSuccess_RoomAvability_ReturnsSuccessResultCode( bool roomAvailability)
+        {
+            if (!roomAvailability)
+            {
+                _availableStudyRoom.Clear();
+            }
+            return _bookingService.BookStudyRoom(_request).Code;
+
+            
         }
 
     }
